00b235da5fa538bbac74c1c73719bb473b3cec77
SaaKlebjeb <raksada25@gmail.com>
2025-03-17 22:45:25 +0700

    hehe

--- src/ReviewFetchData.jsx
+++ src/ReviewFetchData.jsx
@@ -140,6 +140,39 @@ export default function ReviewFetchData() {
             console.error("Error:", err);
         }
     }
+
+    //delete
+    const HandleDelete=async(id)=>{
+        try{
+            const res = await fetch('https://api.jsonbin.io/v3/b/67d238f08960c979a57077c0/latest', {
+                method: 'GET',
+                headers: {
+                    "Accept": "application/json",
+                    "X-Master-Key": '$2a$10$.qByQ.x2uNIFXkvsNYretuA2eAsncorzoSs3gO2/7w7BVERmxfUfi'
+                }
+                
+            });
+    
+            const data = await res.json();
+            let CurrentData = data.record?.users || []; // ✅ Ensure it's an array
+           
+            const DeletedData=CurrentData?.filter((item)=>item.id!==id) || [];
+
+            await fetch('https://api.jsonbin.io/v3/b/67d238f08960c979a57077c0',{
+                method:'PUT',
+                headers:{
+                    'Content-Type':'application/json',
+                    "X-Master-Key":'$2a$10$.qByQ.x2uNIFXkvsNYretuA2eAsncorzoSs3gO2/7w7BVERmxfUfi'
+                },
+                body:JSON.stringify({users:DeletedData})
+            })
+            setData(DeletedData)
+
+        }
+        catch(err){
+            console.log('Message error:',err);
+        }
+    }
     
   return (
     <>
@@ -184,7 +217,7 @@ export default function ReviewFetchData() {
                                 <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                                </svg>
 
-                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 m-2 text-red-500 active:scale-110 hover:cursor-pointer">
+                               <svg onClick={()=>HandleDelete(item.id)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7 m-2 text-red-500 active:scale-110 hover:cursor-pointer">
                                 <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                                </svg>
 
